4d09d83a512abc6e45c95f2ebc6681a0f2af7b4a
akarishav <101082244+akarishav@users.noreply.github.com>
2022-03-14 15:55:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main (String[]args){
+        // Anything we write between " " that will show as output 
         System.out.println("Hello World");
     }
 }
